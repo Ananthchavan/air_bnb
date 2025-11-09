97a69d3be459b8a83fd545b924b7b051d1e320aa
AnanthChavan <ananthchavan2006@gmail.com>
2025-11-09 22:48:52 +0530

    added show listings route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,23 +18,31 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("views engine" , "ejs");
+app.set("views" , path.join(__dirname,"views"));
+
 app.get("/" , (req,res) => {
     res.send("hi, I am root");
 });
 
-app.get("/testlistings" , async (req,res) => {
-    let sampleListing = new Listing({
-        title: "Sample Listing",
-        description: "This is a sample listing description.",
-        price: 100,
-        location: "Sample Location",
-        country: "Sample Country"
-    });
+app.get("/listings" , async(req,res) => {
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs" , {allListings});
+});
 
-    await sampleListing.save();
-    console.log("Sample listing saved to database");
-    res.send("Sample listing created and saved to database");
-})
+//    app.get("/testlistings" , async (req,res) => {
+//        let sampleListing = new Listing({
+//            title: "Sample Listing",
+//            description: "This is a sample listing description.",
+//           price: 100,
+//            location: "Sample Location",
+//            country: "Sample Country"
+//        });
+//
+//        await sampleListing.save();
+//        console.log("Sample listing saved to database");
+//        res.send("Sample listing created and saved to database");
+//    })
 
 app.listen(8080 , () => {
     console.log("Server is running on port 8080");
