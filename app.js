4d4f706f9e944d678014df998afa6aa05c6c8947
AnanthChavan <ananthchavan2006@gmail.com>
2025-11-20 18:25:46 +0530

    added joi t improve validations

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -33,6 +34,16 @@ app.get("/" , (req,res) => {
     res.send("hi, I am root");
 });
 
+const validateListing = (req,res,next) => {
+        let {error} = listingSchema.validate(req.body);
+        if(error){
+           let errMsg = error.details.map(el => el.message).join(", "); 
+            throw new ExpressError(400 , error);
+        } else {
+            next();
+        }
+}
+
 //index route
 app.get("/listings" , wrapAsync(async(req,res) => {
     const allListings = await Listing.find({});
@@ -52,11 +63,7 @@ app.get("/listings/:id" , wrapAsync(async (req,res) => {
 }));
 
 //create route
-app.post("/listings" , wrapAsync(async(req,res,next) =>  {
-        if (!req.body.listing || Object.keys(req.body.listing).length === 0) {
-            throw new ExpressError(400, "Send valid data for listing");
-        }
-
+app.post("/listings", validateListing , wrapAsync(async(req,res,next) =>  {
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         res.redirect("/listings");
@@ -70,11 +77,7 @@ app.get("/listings/:id/edit" , wrapAsync(async (req,res) => {
 }));
 
 //update route
-app.put("/listings/:id" , wrapAsync(async(req,res) => {
-    if (!req.body.listing || Object.keys(req.body.listing).length === 0) {
-            throw new ExpressError(400, "Send valid data for listing");
-        }
-
+app.put("/listings/:id", validateListing , wrapAsync(async(req,res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id , {...req.body.listing});
     res.redirect(`/listings/${id}`);
@@ -107,10 +110,12 @@ app.use((req, res, next) => {
 
 
 app.use((err, req, res, next) => {
-    err.statusCode = Number(err.statusCode) || 500;
-    err.message = err.message || "Something went wrong";
-    res.status(err.statusCode).send(err.message);
+    const status = err.statusCode || 500;
+    const message = err.message || "Something went wrong";
+
+    res.status(status).render("error.ejs", { err });
 });
+
 
 app.listen(8080 , () => {
     console.log("Server is running on port 8080");
