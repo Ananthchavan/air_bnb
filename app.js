78c2410d05f4dbb04e226c7b89daa4202021a932
AnanthChavan <ananthchavan2006@gmail.com>
2025-12-18 23:00:15 +0530

    Added client and server side verifications for review model

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema , reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -45,6 +45,16 @@ const validateListing = (req,res,next) => {
         }
 }
 
+const validateReview = (req,res,next) => {
+        let {error} = reviewSchema.validate(req.body);
+        if(error){
+           let errMsg = error.details.map(el => el.message).join(", "); 
+            throw new ExpressError(400 , error);
+        } else {
+            next();
+        }
+}
+
 //index route
 app.get("/listings" , wrapAsync(async(req,res) => {
     const allListings = await Listing.find({});
@@ -59,7 +69,7 @@ app.get("/listings/new" , (req,res) => {
 //show route
 app.get("/listings/:id" , wrapAsync(async (req,res) => {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("./listings/show.ejs" , {listing});
 }));
 
@@ -93,7 +103,7 @@ app.delete("/listings/:id" ,wrapAsync(async(req,res) => {
 
 //Reviews
 //Post Route
-app.post("/listings/:id/reviews" , async(req,res) => {
+app.post("/listings/:id/reviews" , validateReview , wrapAsync(async(req,res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -102,7 +112,7 @@ app.post("/listings/:id/reviews" , async(req,res) => {
     await listing.save();
 
     res.redirect(`/listings/${listing._id}`);
-});
+}));
 
 //    app.get("/testlistings" , async (req,res) => {
 //        let sampleListing = new Listing({
