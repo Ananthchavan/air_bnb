b43e42c97bceb90f035db5f1cdbf157ae8772e17
AnanthChavan <ananthchavan2006@gmail.com>
2025-12-19 22:45:15 +0530

    Added delete feature to reviews

--- app.js
+++ app.js
@@ -114,6 +114,15 @@ app.post("/listings/:id/reviews" , validateReview , wrapAsync(async(req,res) => 
     res.redirect(`/listings/${listing._id}`);
 }));
 
+//delete Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req,res) => {
+    let {id,reviewId} = req.params;
+
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
 //    app.get("/testlistings" , async (req,res) => {
 //        let sampleListing = new Listing({
 //            title: "Sample Listing",
