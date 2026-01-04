2f407bfafc0929e0b76a078ec708a1734a2116ce
AnanthChavan <ananthchavan2006@gmail.com>
2026-01-04 18:17:22 +0530

    Added validate review as a middleware

--- middleware.js
+++ middleware.js
@@ -37,3 +37,13 @@ module.exports.validateListing = (req,res,next) => {
             next();
         }
 };
+
+module.exports.validateReview = (req,res,next) => {
+        let {error} = reviewSchema.validate(req.body);
+        if(error){
+           let errMsg = error.details.map(el => el.message).join(", "); 
+            throw new ExpressError(400 , error);
+        } else {
+            next();
+        }
+};
--- routes/review.js
+++ routes/review.js
@@ -5,16 +5,7 @@ const ExpressError = require("../utils/ExpressError.js");
 const {listingSchema , reviewSchema} = require("../schema.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-
-const validateReview = (req,res,next) => {
-        let {error} = reviewSchema.validate(req.body);
-        if(error){
-           let errMsg = error.details.map(el => el.message).join(", "); 
-            throw new ExpressError(400 , error);
-        } else {
-            next();
-        }
-}
+const {validateReview} = require("../middleware.js");
 
 //Post Route
 router.post("/" , validateReview , wrapAsync(async(req,res) => {
