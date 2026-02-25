624857c7573ed2b742aa91cab30e97ee960a6386
AnanthChavan <ananthchavan2006@gmail.com>
2026-02-25 17:40:58 +0530

    Done with implementation of map services

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,14 @@
 mapboxgl.accessToken = mapToken;
+
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: [77.5937, 12.9719], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
 
+
+
+const marker = new mapboxgl.Marker({ color: "red"})
+    .setLngLat(coordinates)
+    .addTo(map);
