e8f4ffc02edd21c3d4b715e1ab575d80f92ac8a3
AnanthChavan <ananthchavan2006@gmail.com>
2026-02-25 17:26:11 +0530

    Added mechanism to store coordinates

--- controllers/listings.js
+++ controllers/listings.js
@@ -35,7 +35,11 @@ module.exports.createListing = async(req,res,next) =>  {
         const newListing = new Listing(req.body.listing);
         newListing.owner = req.user._id;
         newListing.image = {url , filename};
-        await newListing.save();
+
+        newListing.geometry = response.body.features[0].geometry;
+
+        let savedListing = await newListing.save();
+        console.log(savedListing);
         req.flash("sucess" , "New Listing Created!");
         res.redirect("/listings");
 };
